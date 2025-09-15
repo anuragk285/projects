03907f1195a94ad979d7e24592e92e2b67d13f5b
anuragk285 <anuragkanukuntla009@gmail.com>
2025-09-15 16:14:29 +0530

    Rain alerting-bot through SMS to personal mobile number

---
+++ rain_alert_project/alert.py
@@ -0,0 +1,43 @@
+import requests
+from twilio.rest import Client
+import os
+
+# paste your open weather map api key, here i just hided my api and auth token details environmental variables
+auth_token = os.environ.get("AUTH_TOKEN")
+api_key = os.environ.get("OWM_API_KEY")
+
+OWM_endpoind = "https://api.openweathermap.org/data/2.5/forecast"
+# paste you open weather map account sid
+account_sid = "AC60ef5be39daaa29cf7f944466dff2011"
+
+from_number = "paste your twilio account generated number"
+to_number = "paste your personal mobile number (Ex: '+91123456789')"
+parameters = {
+    # put you place co-ordinates
+    "lat" :17.385044, # lattitude 
+    "lon" :78.486671, # longitude
+    "appid": api_key,
+}
+response = requests.get(url=OWM_endpoind, params=parameters)
+weather_data = response.json()
+
+will_it_rain = False
+
+for i in range(4):
+    hour_data = weather_data["list"][i]
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_it_rain = True
+
+if will_it_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It's going to rain today, Remember to carry an ☔️",
+            from_=from_number,
+            to=to_number,
+        )
+    print(message.status)
+
+
+
